3bcf0770e58af933b65531a3dc60b84e9cbd9492
Atte Torri <atte@torri.be>
2020-02-09 16:40:19 +0100

    bugfixed vec3

--- vec3.cpp
+++ vec3.cpp
@@ -13,31 +13,28 @@ Vec3::Vec3(const Vec3& o) {
 }
 
 Vec3& Vec3::operator+(const Vec3& o) const {
-  x_ = x + o.x;
-  y_ = y + o.y;
-  z_ = z + o.z;
-  Ve3 result = new Vec3(x_, y_, z_);
-  return result;
+  float x_ = x + o.x;
+  float y_ = y + o.y;
+  float z_ = z + o.z;
+  return *(new Vec3(x_, y_, z_));
 }
 
 Vec3& Vec3::operator-(const Vec3& o) const {
-  x_ = x - o.x;
-  y_ = y - o.y;
-  z_ = z - o.z;
-  Vec3 result = new Vec3(x_, y_, z_);
-  return result;
+  float x_ = x - o.x;
+  float y_ = y - o.y;
+  float z_ = z - o.z;
+  return *(new Vec3(x_, y_, z_));
 }
 
 float Vec3::operator*(const Vec3& o) const {
-  x_ = x * o.x;
-  y_ = y * o.y;
-  z_ = z * o.z;
+  float x_ = x * o.x;
+  float y_ = y * o.y;
+  float z_ = z * o.z;
   return x_ + y_ + z_;
 }
 
 Vec3& Vec3::operator*(const float s) const {
-  Vec3 result = new Vec3(s*x, s*y, s*z);
-  return result;
+  return *(new Vec3(s*x, s*y, s*z));
 }
 
 void Vec3::operator+=(const Vec3& o) {
